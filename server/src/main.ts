dfefb1cd1a8d603f8c2423886db0b6e9e63690b9
Gamberge <cedric.righi@gmail.com>
2024-12-04 17:09:36 +0100

    finish quest 2

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,12 +5,19 @@ import "dotenv/config";
 // Note: This is optional and can be removed if the database connection
 // is not required when starting the application
 import "../database/checkConnection";
+import type { RequestHandler } from "express";
 
 // Import the Express application from ./app
 import app from "./app";
 
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
+
+const mainRoot: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series");
+};
+
+app.get("/", mainRoot);
 
 // Start the server and listen on the specified port
 app
